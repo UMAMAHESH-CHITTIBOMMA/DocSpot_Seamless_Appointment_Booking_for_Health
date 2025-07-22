003588007a32d45580a60b4f10e34f1ac223eedf
UMAMAHESH-CHITTIBOMMA <chittibommaumamahesh@gmail.com>
2025-07-22 20:09:05 +0530

    Delete Seamless-Appointment-Booking-for-Health-main/project files directory

--- Seamless-Appointment-Booking-for-Health-main/project files/frontend/src/components/common/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const userData = localStorage.getItem("userData");
-
-  return userData ? children : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
